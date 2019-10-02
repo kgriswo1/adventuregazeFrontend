93329371766a6935bf1552c2db804c49d7f0a8e4
kgriswo1 <kgriswo1@binghamton.edu>
2019-10-02 16:06:50 -0400

    styled display page

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    const body = document.querySelector("body")
     const section = document.querySelector(".section")
     const welcome_container = document.querySelector("#welcome-container")
-    const container = document.querySelector("#container")
     const form_create_user = document.querySelector("#create-user")
+    let container = document.querySelector("#container");
     let user;
     let destination;
 
@@ -23,26 +24,49 @@ document.addEventListener('DOMContentLoaded', (event) => {
             user = data
         })
     
-        section.innerHTML = ''
+        body.innerHTML = ''
         fetch("http://localhost:3000/destinations")
         .then(response => response.json())
         .then(renderDestinations)
     
         function renderDestinations(dests) {
+            let str =  
+            `<nav id="nav-bar">
+                <div class="logo">
+                    <h4>AdventureGaze</h4>
+                </div>
+                <ul class="nav-links">
+                    <li>
+                        <a href="#" data-action="home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" data-action="profile">Profile</a>
+                    </li>    
+                </ul>
+            </nav>`
+            body.insertAdjacentHTML("beforeend", str)
+            container = document.createElement("DIV")
+            container.id = "container"
+            document.body.appendChild(container)
             dests.forEach(renderOneDestination)
         }
+        // <div class="img img1"><img data-id="${dest.id}" src="${dest.img_url}"></div>
+        // <svg class="card__like"  viewBox="0 0 24 24">
 
         function renderOneDestination(dest) {
             let str = 
-            `<div class="tile" data-id=${dest.id}
-            <h1>${dest.location}</h1>
-            <img data-id="${dest.id}"src="${dest.img_url}">
+            `<div class="card" data-id=${dest.id}>
+                <div><img data-id="${dest.id}" src="${dest.img_url}"></div>
+                <div class="card__info">
+                    <h3 class="card__title">${dest.location}</h3>
+                </div>
             </div>`
             container.insertAdjacentHTML("beforeend", str)
         }
     })
 
-    container.addEventListener("click", (event) => {
+    body.addEventListener("click", (event) => {
+        // debugger
         if(event.target.tagName === "IMG") {
             container.innerHTML = ''
             fetch(`http://localhost:3000/destinations/${event.target.parentElement.dataset.id}`)
@@ -52,13 +76,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
             function showDestination(dest) {
                 let str = 
                 `<div class="show" data-id=${dest.id}>
-                <h3>${dest.location}</h3>
-                <img data-id="${dest.id}"src="${dest.img_url}"></img>
-                <p>${dest.description}</p>
-                <form class="select_flight">
-                    <input type="date" name="name" min="2019-04-03" max="2019-12-31">
-                    <input type="submit" name="submit" value="Enter" data-action="create_flight" data-id="${dest.id}">
-                </form>
+                    <h3>${dest.location}</h3>
+                    <img data-id="${dest.id}"src="${dest.img_url}"></img>
+                    <p>${dest.description}</p>
+                    <form class="select_flight">
+                        <input type="date" name="name" min="2019-04-03" max="2019-12-31">
+                        <input type="submit" name="submit" value="Enter" data-action="create_flight" data-id="${dest.id}">
+                    </form>
                 </div>`
                 container.insertAdjacentHTML("beforeend", str)
                 form_select_flight = document.querySelector(".select_flight");
@@ -99,3 +123,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
     })
 
 })
+
